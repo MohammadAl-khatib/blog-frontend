9e81300d5bc1788ffa4b2513d4c2b100b9d04a3a
MohammadAl-khatib <alk20489@gmail.com>
2024-08-03 15:31:27 +0300

    styling for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,61 @@
 import Link from "next/link";
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { UserContext } from "@/contexts/UserContext";
+import { FaFileAlt, FaSignInAlt, FaSignOutAlt, FaUserPlus } from "react-icons/fa";
+import styles from "./Header.module.scss";
 
 export default function Header() {
   const { userData, setUserData } = useContext(UserContext);
   const { name } = userData;
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        {!name && (
-          <>
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <div className={styles.homeSection}>
+          <Link
+            href="/"
+            className={styles.homeLink}
+          >
+            <FaFileAlt className={styles.homeIcon} />
+            <span className={styles.platformName}>Blogify</span>
+          </Link>
+          <span className={styles.description}>Ideas Worth Reading</span>
+        </div>
+        <ul className={styles.navList}>
+          {!name && (
+            <>
+              <li>
+                <Link
+                  href="/login"
+                  onClick={() => setUserData({ name: "Mock User" })}
+                  className={styles.navLink}
+                >
+                  <FaSignInAlt className={styles.navIcon} /> Login
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/register"
+                  className={styles.navLink}
+                >
+                  <FaUserPlus className={styles.navIcon} /> Register
+                </Link>
+              </li>
+            </>
+          )}
+          {name && (
             <li>
-              <Link href="/login" onClick={() => setUserData({ name: "Mock User" })}>
-                Login
+              <Link
+                href="/"
+                onClick={() => setUserData({})}
+                className={styles.navLink}
+              >
+                <FaSignOutAlt className={styles.navIcon} /> Logout
               </Link>
             </li>
-            <li>
-              <Link href="/register">Register</Link>
-            </li>
-          </>
-        )}
-        {name && (
-          <li>
-            <Link href="/" onClick={() => setUserData({})}>
-              Logout
-            </Link>
-          </li>
-        )}
-      </ul>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 }

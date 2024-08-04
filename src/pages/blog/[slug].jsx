5b4daf2596a2c380e32cbbfdd23a3c9149020b16
MohammadAl-khatib <alk20489@gmail.com>
2024-08-04 20:54:49 +0300

    show modal after publish

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,10 +1,19 @@
 import { useRouter } from "next/router";
 import React from "react";
 
-export default function Blog() {
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const repo = await res.json();
+  // Pass data to the page via props
+
+  return { props: { repo } };
+}
+
+export default function Blog({ repo }) {
   const {
     query: { slug },
   } = useRouter();
 
-  return <div>Blog {slug} Page</div>;
+  return <div>Blog {repo.stargazers_count} Page {slug}</div>;
 }
--- src/pages/create/index.jsx
+++ src/pages/create/index.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import styles from "./Create.module.scss";
 import dynamic from "next/dynamic";
+
+import Modal from "@/components/Modal/Modal";
+import styles from "./Create.module.scss";
 
 const Editor = dynamic(() => import("../../components/Editor/Editor"), {
   ssr: false, // don't render server side, this uses document
 });
 
-async function createNewPost({ event, title, summary, file, content }) {
+async function createNewPost({ event, title, summary, file, content, setModalMessage, setShowModal }) {
   event.preventDefault();
 
   const data = new FormData();
@@ -15,11 +17,19 @@ async function createNewPost({ event, title, summary, file, content }) {
   data.set("content", content);
   data.set("file", file);
 
-  await fetch("http://localhost:4000/post", {
+  const response = await fetch("http://localhost:4000/post", {
     method: "POST",
     body: data,
     credentials: "include",
   });
+
+  if (response.status === 200) {
+    const blogData = await response.json();
+    setModalMessage(`${blogData.title} has been published successfully`);
+  } else {
+    setModalMessage("Something went wrong, try again later");
+  }
+  setShowModal(true);
 }
 
 export default function CreatePost() {
@@ -27,11 +37,13 @@ export default function CreatePost() {
   const [summary, setSummary] = useState("");
   const [file, setFIle] = useState("");
   const [content, setContent] = useState("");
+  const [modalMessage, setModalMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className={styles.container}>
       <form
-        onSubmit={(event) => createNewPost({ event, title, summary, file, content })}
+        onSubmit={(event) => createNewPost({ event, title, summary, file, content, setModalMessage, setShowModal })}
         className={styles.form}>
         <input
           type="title"
@@ -60,6 +72,12 @@ export default function CreatePost() {
         />
         <button className={styles.button}>Publish Post</button>
       </form>
+      {showModal && (
+        <Modal
+          message={modalMessage}
+          onClose={() => setShowModal(false)}
+        />
+      )}
     </div>
   );
 }

38efaaa3fc7255b58fcdcdd57832745e392600fb
MohammadAl-khatib <alk20489@gmail.com>
2024-08-04 22:59:38 +0300

    blog page

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,19 +1,35 @@
-import { useRouter } from "next/router";
 import React from "react";
+import { formatISO9075 } from "date-fns";
+import styles from "./Blog.module.scss";
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ params }) {
   // Fetch data from external API
-  const res = await fetch("https://api.github.com/repos/vercel/next.js");
-  const repo = await res.json();
-  // Pass data to the page via props
+  const res = await fetch(`http://localhost:4000/post/${params.slug}`);
+  const data = await res.json();
 
-  return { props: { repo } };
+  // Pass data to the page via props
+  return { props: { data } };
 }
 
-export default function Blog({ repo }) {
-  const {
-    query: { slug },
-  } = useRouter();
+export default function Blog({ data }) {
+  const { title, createdAt, author, cover, content } = data;
 
-  return <div>Blog {repo.stargazers_count} Page {slug}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.heading}>{title}</h1>
+      <time className={styles.createdAt}>{formatISO9075(new Date(createdAt))}</time>
+      <div className={styles.author}>by @{author.username}</div>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src={`http://localhost:4000/${cover}`}
+          alt=""
+        />
+      </div>
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </div>
+  );
 }

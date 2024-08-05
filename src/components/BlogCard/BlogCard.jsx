6d823f75af5a275cc637946dd5b58d9f0c768b4a
MohammadAl-khatib <alk20489@gmail.com>
2024-08-06 00:44:24 +0300

    edit button

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 import { formatISO9075 } from "date-fns";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 import styles from "./BlogCard.module.scss";
+import { UserContext } from "@/contexts/UserContext";
 
 export default function BlogCard({ _id, cover, title, author, createdAt, summary }) {
-  const imageUrl = cover ? `http://localhost:4000/${cover}` : "https://via.placeholder.com/340x200/eeeeee/cccccc?text=Image+Placeholder";  
+  const {
+    userData: { username },
+  } = useContext(UserContext);
+
+  const imageUrl = cover ? `http://localhost:4000/${cover}` : "https://via.placeholder.com/340x200/eeeeee/cccccc?text=Image+Placeholder";
 
   return (
     <div className={styles.post}>
@@ -17,6 +23,11 @@ export default function BlogCard({ _id, cover, title, author, createdAt, summary
             alt="blog cover"
           />
         </Link>
+        {author.username === username && (
+          <Link className={styles.editButton} href={`/edit/${_id}`}>
+            <FaEdit />
+          </Link>
+        )}
       </div>
       <div className={styles.texts}>
         <Link href={`/blog/${_id}`}>
